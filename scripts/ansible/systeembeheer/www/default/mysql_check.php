7f8d5118467df9d3ff14f108ba83eac08a91f23d
Elewout Vercaeren <elewout.vercaeren@student.ucll.be>
2019-12-11 19:23:16 +0100

    ansible: ansible scripts and configs for nginx and ntp

---
+++ scripts/ansible/systeembeheer/www/default/mysql_check.php
@@ -0,0 +1,25 @@
+ <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT text FROM check.log order by date desc limit 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo  $row["text"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?> 
